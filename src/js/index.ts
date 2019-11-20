7eb0206a2a6d305019758918d4024101d9f9a51d
LucasvanNiekerk <43374913+LucasvanNiekerk@users.noreply.github.com>
2019-11-20 13:21:54 +0100

    Add files via upload

---
+++ src/js/index.ts
@@ -0,0 +1,12 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person): string {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+let user: Person = { firstName: "John", lastName: "Doe" };
+
+let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+element.innerHTML = greeter(user);
