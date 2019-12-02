e42deab87b3e1f5c9205d46d35f327655504b0d0
danielegtved <43374944+danielegtved@users.noreply.github.com>
2019-12-02 11:21:03 +0100

    chart

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,7 +2,7 @@ import axios, {
     AxiosResponse,
     AxiosError
 } from "../../node_modules/axios/index";
-
+import { BorderWidth, Chart, Point, ChartColor } from 'chart.js';
 
 //
 // Interfaces
@@ -101,6 +101,50 @@ let popupElement: HTMLDivElement = <HTMLDivElement>document.getElementById("rasp
 let raspberryIdErrorDivOutputElement: HTMLDivElement = <HTMLDivElement>document.getElementById("raspberryIdErrorOutput");
 
 let raspberryIdInputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("raspberryIdInput");
+
+let chart: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("chart");
+
+//
+// Chart
+//
+
+var myChart = new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
 //
 // Buttons
