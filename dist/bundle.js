8e2a1d97dc49d755ab5e0bc7ae502b3efc119e4c
Morten <morto1995@hotmail.com>
2019-12-05 13:54:55 +0100

    files deleted and replaced

--- dist/bundle.js
+++ dist/bundle.js
@@ -35881,6 +35881,8 @@ var temperatureAnnotation;
 var raspberryId = "";
 // The City for the external temeperature. This information is saved in localStorage with the key "currentCity".
 var currentCity = "";
+// boolean wether the popup is active or not
+var popupActive = false;
 // This is run after the page has loaded. Here we get the data to show and load localStorage.
 window.onload = onloadMethods;
 // The baseUri for our web Api. For more information regarding the Api visit "https://weatherstationrest2019.azurewebsites.net/api/help/index.html".
@@ -35917,6 +35919,8 @@ cityDropDownElement.addEventListener("change", changeCity);
 var prognosisday1 = document.getElementById("prognosisDay1");
 var prognosisday2 = document.getElementById("prognosisDay2");
 var prognosisday3 = document.getElementById("prognosisDay3");
+var label1 = document.getElementById("label1");
+var label2 = document.getElementById("label2");
 //
 // Chart
 //
@@ -36006,17 +36010,17 @@ function getRangeOfDay(date, index) {
             avgTemperature = resultTemperature / i;
             avgHumidity = resultHumidity / i;
         }
+        console.log("before: " + tempDate);
         var tType = " °C";
-        tableStringArray[index + 1] = "<tr> <td>" + tempDate + "</td><td>" + (Math.round(avgTemperature * 10) / 10) + tType + "</td><td>" + (Math.round(avgHumidity * 10) / 10) + "%" + "</td> </tr>";
+        tableStringArray[index + 1] = "<tr> <th>" + tempDate + "</th><td>" + (Math.round(avgTemperature * 10) / 10) + tType + "</td><td>" + (Math.round(avgHumidity * 10) / 10) + "%" + "</td> </tr>";
         if (index > 5) {
             tableStringArray[8] = "</tbody>";
-            console.log(tableStringArray.join(""));
             getAllOutputTable.innerHTML = tableStringArray.join("");
         }
         //console.log("temp: " + avgTemperature);
         //console.log("hum: " + avgHumidity);
         //console.log(index);
-        //console.log(tempDate);
+        console.log(tempDate);
         myChart.data.datasets[0].data[index] = avgTemperature;
         myChart.data.datasets[1].data[index] = avgHumidity;
         myChart.update();
@@ -36050,9 +36054,11 @@ olderDataButton.addEventListener("click", displayOlderData);
 function onloadMethods() {
     setTimeout(function () {
         //localStorage.clear();
-        fillDropDown();
         browserStorage();
-        loadData();
+        fillDropDown();
+        if (localStorage.getItem("raspId") != null) {
+            loadData();
+        }
     }, 10);
 }
 function browserStorage() {
@@ -36073,26 +36079,30 @@ function browserStorage() {
             temperatureAnnotation = "Celsius";
             localStorage.setItem("temperatureType", temperatureAnnotation);
         }
+        fixMortensbuttons();
         //To check what city the user wants to see information from.
         if (localStorage.getItem("currentCity") != null) {
             currentCity = localStorage.getItem("currentCity");
         }
         else {
             currentCity = "Roskilde%20Kommune";
-            cityDropDownElement.options[0].selected = true;
             localStorage.setItem("currentCity", currentCity);
         }
-        console.log("RaspberryId: " + raspberryId);
-        console.log("Temperature annotion: " + temperatureAnnotation);
-        console.log("current city:" + currentCity);
-        console.log("Local storage raspberry id: " + localStorage.getItem("raspId"));
-        console.log("Local storage temperature annotation: " + localStorage.getItem("temperatureType"));
-        console.log("Local storage current city: " + localStorage.getItem("currentCity"));
     }
     //If localStorage is not supported we tell the client. 
     else {
         NoLocalStorageOutputElement.innerHTML = "Your browser does not support local storage (inspect page for more information).";
         console.log("Webstorage is supported by (minimun version): Google Chrome v4.0, Microsoft Edge v8.0, Firefox v3.5, Safari v4.0 and Opera v11.5");
+    }
+}
+function fixMortensbuttons() {
+    if (temperatureAnnotation === "Celsius") {
+        label1.className += " active";
+        label2.className = label2.className.replace(/(?:^|\s)active(?!\S)/g, '');
+    }
+    else {
+        label2.className += " active";
+        label1.className = label1.className.replace(/(?:^|\s)active(?!\S)/g, '');
     }
 }
 function displayFrontpage() {
@@ -36253,6 +36263,11 @@ function fillDropDown() {
         option.text = cities[index];
         cityDropDownElement.add(option);
     }
+    for (var index = 0; index < apiNames.length; index++) {
+        if (apiNames[index] === currentCity) {
+            cityDropDownElement.selectedIndex = index;
+        }
+    }
 }
 function loadData() {
     getLatestWeatherInformation(internalTemperatureOutputElement, "Temperature");
@@ -36315,6 +36330,7 @@ function convertToFahrenheit(temp) {
     return (Number(temp) * (9 / 5) + 32).toFixed(1);
 }
 function convertToCelcius(temp) {
+    // tC = (tF -32) / (9 / 5) 
     return ((Number(temp) - 32) / (9 / 5)).toFixed(1);
 }
 function loadApiData() {
@@ -36323,9 +36339,11 @@ function loadApiData() {
 }
 function openRaspberryIdPopup() {
     popupElement.style.display = "block";
+    popupActive = true;
 }
 function closeRaspberryIdPopup() {
     popupElement.style.display = "none";
+    popupActive = false;
 }
 //
 // OpenWeatherMap API models. (We only use small part).
